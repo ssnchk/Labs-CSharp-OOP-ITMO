9fca241f6b8f31a290ad49767a821ea420a5e55d
ssnchk <mrsiznpro@gmail.com>
2024-11-09 13:42:37 +0300

    wfn

---
+++ src/Lab3/Messages/MessageStatus.cs
@@ -0,0 +1,18 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab3.ResultTypes;
+
+namespace Itmo.ObjectOrientedProgramming.Lab3.Messages;
+
+public class MessageStatus
+{
+    public bool IsRead { get; private set; } = false;
+
+    public MarkAsReadResult MarkAsRead()
+    {
+        if (IsRead)
+            return new MarkAsReadResult.Failure("Message is already read");
+
+        IsRead = true;
+
+        return new MarkAsReadResult.Success();
+    }
+}
